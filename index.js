1ab83146b5646e8fa5862e091cdf31cf18937a71
Wilhelm Grosz <wilhelm.grosz@iquestgroup.com>
2019-02-18 15:37:42 +0200

    updates code so that it's even more sofisticated :D

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { Subject } from 'rxjs';
-const cat$_stream = new Subject()
-const dog_stream = new Subject()
-cat$_stream.subscribe(function(cats){
-    dog_stream.subscribe(function(dogs){
+function load(Subject) {
+  const cat$_stream = new Subject()
+  const dog_stream = new Subject()
+  cat$_stream.subscribe(function(cats){
+        dog_stream.subscribe(function(dogs){
       cats.forEach(function(cat){
         cat === 'Fluffykins' ? console.log('Fluffykins is a tiny tiny dragon that spits lighning') : '' === '' ? console.log('No Fluffykins found continuing') : '' 
         dogs.forEach(function(dog){
@@ -11,6 +11,9 @@ cat$_stream.subscribe(function(cats){
       })
     })
   })
-cat$_stream.next([ 'Gabor', 'Stuff', "Fluffykins", 'Jingo', "Miuriel" ])
-dog_stream.next([ "Ionas", 'Jingo', 'Mauro', 'LOL', "Fluffykins"]);
+  cat$_stream.next([ 'Gabor', 'Stuff', "Fluffykins", 'Jingo', "Miuriel" ])
+  dog_stream.next([ "Ionas", 'Jingo', 'Mauro', 'LOL', "Fluffykins"]);}
 
+import { Subject } from 'rxjs';
+
+load(Subject);
