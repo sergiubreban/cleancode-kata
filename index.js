950f927afe3f3f130ba0e317e73f7907c7466593
Sergiu Breban <sergiu.breban@iquestgroup.com>
2019-02-18 13:43:15 +0200

    created namespace 1

---
+++ index.js
@@ -0,0 +1,25 @@
+let CLEANROOM = function () {
+  return new Promise(function(resolve, reject) {
+    resolve('Cleaned the room');
+  });
+}
+
+let removegarbage = function (message) {
+  return new Promise(function(resolve, reject) {
+    resolve(`${message} Removed garbage`);
+  });
+}
+
+let WinIcecream = function (message) {
+  return new Promise(function(resolve, reject) {
+    resolve(`${message} Won Icecream`);
+  });
+}
+
+CLEANROOM().then(function(res) {
+  removegarbage(res).then(function(res){
+    WinIcecream(res).then(function(){
+      console.log('yeeeeey')
+    })
+  })
+})
