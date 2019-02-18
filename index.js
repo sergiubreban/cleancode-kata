f11722dccdaa1e3453984530151e4df864caca2d
Sergiu Breban <sergiu.breban@iquestgroup.com>
2019-02-18 15:08:12 +0200

    updated README

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
+var response = 0;
 function add(arr) {
-  let response = 0;
-
   for (let index = 0, len = arr.length; index < len; index++) {
     if (typeof arr[index] !== 'number') {
       response = response + add(arr[index])
@@ -8,8 +7,6 @@ function add(arr) {
       response = response + arr[index]
     }
   }
-
   return response;
 }
-
 console.log(add([1, [2, [3, 4], [5, 6, 7, 8], 9, 10]]));
